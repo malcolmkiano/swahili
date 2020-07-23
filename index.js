703e053057b9378a32ae7e9cfbcca3f0ef23f421
Malcolm Kiano <malcolmkiano@gmail.com>
2020-07-22 23:42:57 -0400

    feat: add basic variable and expression parsing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Stuff goes in here
+const run = require('./swh');
+
+let lines = [
+  'let x = 12.5 + 5.0 + 6;',
+  'let y = m * x + b;',
+  'let y = greeting + "world";',
+  'let y = "x";',
+  'let f = 6.9;',
+  'x = "13";',
+  'abc = "hi"',
+  'fn helloWorld(name):',
+  'let f5 = null + true;'
+];
+
+run(lines);
